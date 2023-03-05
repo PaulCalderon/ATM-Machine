37ec11b39c27c807b658a1f93aa9918fc0b11a32
PaulCalderon <calderon.paul.august@gmail.com>
2023-03-05 20:57:50 +0800

    renamed main.py to ATM_API.py

---
+++ ATM_API.py
@@ -0,0 +1,209 @@
+import sys
+import os
+import getopt
+import configparser
+from ATM_Repository import AtmCommands
+
+CONFIG = "config.ini"
+
+config_data = configparser.ConfigParser()
+config_data.read(CONFIG)
+config_option = config_data["options"]
+for data in config_option: # gets config option for session_file
+    if data == 'session_file':
+        SESSION_FILE = config_option.get(data)
+
+
+def check_input(command: list) -> str | None:  # should raise exception if error
+    """parses and checks CLI input"""
+
+    if os.path.exists(SESSION_FILE): #checks if session file exists
+        command_list = ["login", "create", "help", "withdraw", "deposit", "check", "change", "pay", "logout"]
+    else:
+        command_list = ["login", "create", "help"]
+
+    if command.lower() in command_list:
+        return command.lower()
+    else:
+        raise ValueError("Command is invalid")
+
+def command_parser(command: str, *flags: list | None) -> None:
+    """has logic for selecting correct action based on parsed command"""
+    if len(flags) > 0:
+        flags = list(flags[0])
+            #converts tuple to list. this is because of *flags to facilitate variable arguemnts
+    match command:
+        case 'login':
+            try:
+                if len(flags) != 4:
+                    raise ValueError
+                flag, args = getopt.getopt(flags, "u:p:")
+
+            except:
+                print("Syntax should be login -u <ID> -p <PIN> ")
+                raise ValueError("Incorrect Flags")
+
+            for option, argument in flag:
+                if option == '-n':
+                    account_id = argument
+                elif option == '-p':
+                    pin = argument
+            AtmCommands.login(account_id, pin)
+            print("Login Complete")
+
+        case 'create': #need some workaround to create transaction entry for this
+            try:
+                if len(flags) != 6:
+                    raise ValueError
+
+                flag, args = getopt.getopt(flags, "n:l:p:")
+            except:
+                print("Syntax should be create -n <name> -l <last name> -p <PIN> ")
+                raise ValueError("Incorrect Flags")
+
+            for option, argument in flag:  #checks if PIN is 6 digits long and numeric
+                if option == '-p':
+                    if len(argument) != 6 or not argument.isnumeric():
+                        raise ValueError("PIN should be 6 digits long")
+
+            for option, argument in flag:
+                if option == '-n':
+                    name = argument
+                elif option == '-l':
+                    lastname = argument
+                elif option == '-p':
+                    pin = argument
+            AtmCommands.create_account(name, lastname, pin)
+            print("Account Created")
+
+        case 'help':
+            if os.path.exists(SESSION_FILE):
+
+                print("Available Commands:")
+                print("Create - Creates a new account - login -u <ID> -p <PIN>") 
+                print("Login - Login to your account and start a session")
+                print("Withdraw - Withdraw from available balance")
+                print("Deposit - Deposits amount to balance")
+                print("Check - Checks available balance")
+                print("Change - Changes PIN")
+                print("Pay - Pay bills")
+                print("History - Displays past transaction")
+                print("Logout - End current session")
+                print("Help - Displays this help prompt")
+
+            else:
+                print("Available Commands:")
+                print("Create - Creates a new account - main.py login -u <ID> -p <PIN>")
+                print("Login - Login to your account and start a session")
+                print("Help - Displays this help prompt")
+
+        case 'withdraw': #need to be divisible by 100
+            try:
+                if len(flags) != 2:
+                    raise ValueError
+                flag, args = getopt.getopt(flags, "a:")
+            except:
+                raise ValueError("Incorrect Flags")
+
+            for option, argument in flag:  
+                #checks if withdrawl amount is divisible by 100 #Zero is a valid withdrawl amount
+
+                if option == '-a':
+                    if not argument.isnumeric():
+                        raise ValueError("Withdraw amount should divisible by 100")
+
+                    if int(argument) % 100 != 0:
+                        raise ValueError("Withdraw amount should divisible by 100")
+            
+            for option, argument in flag:
+                if option == '-a':
+                    amount = argument
+            account_id, old_balance, new_balance = AtmCommands.withdraw(amount)
+            print("Would you like to print a receipt? (y/n)") 
+            print("Not printing a receipt would display the previous transaction results on your screen")
+            if input() == ('y' / 'Y'):
+                print("Receipt Printed")
+            else:
+                print("Account ID: ", account_id)
+                print("Old Balance: ", old_balance)
+                print("New Balance: ", new_balance)
+            
+            
+            
+        case 'deposit':
+            try:
+                if len(flags) != 2:
+                    raise ValueError
+                flag, args = getopt.getopt(flags, "a:")
+            except:
+                raise ValueError("Incorrect Flags")
+            
+            for option, argument in flag:  
+                #raise exception if amount is non numberic
+                if option == '-a':
+                    if not argument.isnumeric():
+                        raise ValueError("Deposit amount is invalid")
+                    amount = argument
+            
+            AtmCommands.deposit(amount)
+            print("Successfully deposited: ",amount)
+
+        case 'check':
+            amount = AtmCommands.check_balance()
+            #TODO complete display here 
+            print("Current Balance: ", amount)
+
+        case 'change':
+            try:
+                if len(flags) != 2:
+                    raise ValueError
+                flag, args = getopt.getopt(flags, "p:")
+            except:
+                raise ValueError("Incorrect Flags")
+            
+            for option, argument in flag:  #checks if PIN is 6 digits long and numeric
+                if option == '-p':
+                    if len(argument) != 6 or not argument.isnumeric():
+                        raise ValueError("PIN should be 6 digits long")
+            
+            for option, argument in flag:
+                if option == '-p':
+                    pin = argument
+            AtmCommands.change_pin(pin)
+            print("Pin Changed")
+
+        case 'pay':
+            if len(flags) != 2:
+                raise ValueError("Incorrect Flags")
+            try:
+                flag, args = getopt.getopt(flags, "a:")
+            except:
+                raise ValueError("Incorrect Flags")
+            
+            for option, argument in flag:  
+                #raise exception if amount is non numberic
+                if option == '-a':
+                    if not argument.isnumeric():
+                        raise ValueError("Amount is invalid")
+                    amount = argument
+
+            AtmCommands.pay_bills(amount)
+            print("Would you like to print a receipt? (y/n)") 
+            print("Not printing a receipt would display the previous transaction results on your screen")
+            if input() == ('y' / 'Y'):
+                print("Receipt Printed")
+            else:
+                print("Account ID: ", account_id)
+                print("Old Balance: ", old_balance)
+                print("New Balance: ", new_balance)
+
+        case 'history':
+            raise NotImplementedError
+        
+        case 'logout':
+            AtmCommands.logout()
+            print("Session Ended")
+
+if __name__ == "__main__":
+    parsed_command = check_input(sys.argv[1])
+    command_parser(parsed_command, sys.argv[2:])
---
+++ test_ATM_API.py
@@ -0,0 +1,201 @@
+import pytest
+import os
+import configparser
+from main import check_input, command_parser
+
+
+CONFIG = "config.ini"
+
+config_data = configparser.ConfigParser()
+config_data.read(CONFIG)
+option = config_data["options"]
+for data in option: # gets config option for session_file
+    if data == 'session_file':
+        SESSION_FILE = option.get(data)
+
+
+class TestCLIHandlingOfMain:
+    """Class for grouping tests"""
+    def test_program_should_raise_error_if_command_is_invalid(self):
+        """ To be implemented by creating a method in main to check if command is valid"""
+        with pytest.raises(ValueError, match="Command is invalid") as exc_info:
+            check_input("")
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Command is invalid"
+
+    def test_program_commands_should_be_case_insensitive(self):
+        """ Checking method should be case insentive and not raise error"""
+        assert check_input("CREATE")
+        assert check_input("LOgin")
+        assert check_input("HELp")
+
+    def test_program_should_accept_all_commands_as_valid_if_session_file_exists(self):
+        """All commands should be valid"""
+        with open(SESSION_FILE, "w") as file:  #used to create session_file to simulate open session
+            pass
+        assert check_input("logout")
+        assert check_input("Check")
+        assert check_input("Pay")
+        assert check_input("deposit")
+        assert check_input("withdraw")
+        assert check_input("Change")
+
+    def test_program_should_only_accept_login_or_create_account_or_help_or_logout_commands_when_there_is_no_active_session(self):
+        """Will check for prescence of session file before accepting any other commands"""
+        if os.path.exists(SESSION_FILE):
+            os.remove(SESSION_FILE)
+
+        with pytest.raises(ValueError, match="Command is invalid") as exc_info:
+            check_input("Check Balance")
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Command is invalid"
+
+    def test_program_should_raise_value_error_if_flags_are_invalid_create(self):
+        """assumes that check_input is correct"""
+        command = check_input("Create")
+
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, [])
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, ['-u','any','-l','lastname','-p','123456'])
+                #-u is wrong input. main should raise exception
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+    def test_program_should_raise_value_error_if_flags_are_invalid_login(self):
+        """assumes that check_input is correct"""
+        command = check_input("login")
+
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, [])
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, ['-a','any','-l','lastname'])
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+    def test_program_should_raise_value_error_if_flags_are_invalid_withdraw(self):
+        """assumes that check_input is correct"""
+
+        with open(SESSION_FILE, "w") as file:  #used to create session_file to simulate open session
+            pass
+        command = check_input("withdraw")
+
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, [])
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, ['-x','any'])
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+    def test_program_should_raise_value_error_if_flags_are_invalid_deposit(self):
+        """assumes that check_input is correct"""
+
+        with open(SESSION_FILE, "w") as file:  #used to create session_file to simulate open session
+            pass
+        command = check_input("deposit")
+
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, [])
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, ['-x','any'])
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+    def test_program_should_raise_value_error_if_flags_are_invalid_change_pin(self):
+        """assumes that check_input is correct"""
+
+        with open(SESSION_FILE, "w") as file:  #used to create session_file to simulate open session
+            pass
+        command = check_input("change")
+
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, [])
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, ['-x','any']) 
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+    def test_program_should_raise_value_error_if_flags_are_invalid_pay_bills(self):
+        """assumes that check_input is correct"""
+        with open(SESSION_FILE, "w") as file:  #used to create session_file to simulate open session
+            pass
+
+        command = check_input("pay")
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, [])
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+        with pytest.raises(ValueError, match="Incorrect Flags") as exc_info:
+            command_parser(command, ['-x','any'])
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "Incorrect Flags"
+
+    def test_program_should_raise_value_error_if_pin_in_create_account_is_invalid(self):
+        """Checks if pin is valid. Pin should be 6 digits and numeric"""
+        command = check_input("create")
+        with pytest.raises(ValueError, match="PIN should be 6 digits long") as exc_info:
+            command_parser(command, ['-n', 'first', '-l', 'last', '-p','12345'])
+            #PIN is only 5 digits
+        with pytest.raises(ValueError, match="PIN should be 6 digits long") as exc_info:
+            command_parser(command, ['-n', 'first', '-l', 'last', '-p','asdsad'])
+            #PIN isn't only numeric characters
+
+    def test_program_should_raise_value_error_if_pin_in_change_pin_is_invalid(self):
+        """Checks if pin is valid. Pin should be 6 digits and numeric"""
+        command = check_input("change")
+        with pytest.raises(ValueError, match="PIN should be 6 digits long") as exc_info:
+            command_parser(command, ['-p','12345'])
+            #PIN is only 5 digits
+        with pytest.raises(ValueError, match="PIN should be 6 digits long") as exc_info:
+            command_parser(command, ['-p','asdsad'])
+            #PIN isn't only numeric characters
+
+    def test_program_should_raise_value_error_if_withdraw_amount_is_not_divisible_by_100(self):
+        """Exception raised when not divisible by 100 or invalid"""
+        command = check_input("withdraw")
+        with pytest.raises(ValueError, match="Withdraw amount should divisible by 100"):
+            command_parser(command, ['-a', '2304']) #amount is not divisible by 100
+        with pytest.raises(ValueError, match="Withdraw amount should divisible by 100"):
+            command_parser(command, ['-a', 'werwer']) #amount should be numbers only
+
+    def test_program_should_raise_value_error_if_deposit_amount_is_invalid(self):
+        """Exception raised when deposit is invalid"""
+        command = check_input("deposit")
+        with pytest.raises(ValueError, match="Deposit amount is invalid"):
+            command_parser(command, ['-a', '-2304']) #amount should be numberic
+    
+    def test_program_should_raise_value_error_if_pay_bills_amount_is_invalid(self):
+        """Exception raised when pay bill amount is invalid"""
+        command = check_input("pay")
+        with pytest.raises(ValueError, match="Amount is invalid"):
+            command_parser(command, ['-a', '-2304']) #amount should be numberic
+    
+    def test_program_should_be_able_to_display_help_prompt(self):
+        command = check_input("help")
+        command_parser(command) #will raise error if not handled
+        
+    @pytest.mark.skip(reason="no way of currently testing this")
+    def test_program_should_be_able_to_retrieve_history_of_past_transactions(self): #TODO implement
+        pass
+
+    def test_clean_up(self):
+        """dummy test to clean up created files"""
+        if os.path.exists(SESSION_FILE):
+            os.remove(SESSION_FILE)
+
